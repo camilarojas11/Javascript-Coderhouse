384ab33710bde691a9e41c4582d9d0672adbecc3
Camila Rojas <90579964+camilarojas11@users.noreply.github.com>
2021-10-22 19:41:46 -0300

    Modifique y agregue el desafio 2

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
+//Desafio 2
+
 //Variables
-let añoNacimiento = 0;
-let edad = 0;
-let resultado = 0;
+precioSinIva = 0.00;
+totalIva = 0.00;
+precioConIva = 0.00;
 
 //Constante
-const añoActual = 2021;
+const iva27 = 0.27;
+const iva21 = 0.21;
+const iva10 = 0.105;
 
+//Ingreso de Datos
+alert("Bienvenido! Usaremos este programa para calcular el IVA de un producto que usted elija. \nPor favor seleccione la opción deseada");
+let opcion = parseInt(prompt(" 1. Consultar total con IVA del 27% \n 2. Consultar total con IVA del 21% \n 3. Consultar total con IVA del 10,5%"));
 
-//Ingreso de datos del usuario
-alert("Hola! Por favor completa los siguientes datos.");
-let nombre = prompt("Ingrese su nombre");
-let apellido = prompt("Ingrese su apellido");
-añoNacimiento = parseInt(prompt("Por favor ingrese el año en que nació (Ejemplo: 1994)"));
+//Condicionales
 
-//Proceso
-edad = añoActual - añoNacimiento;
+if ((opcion != 1) && (opcion != 2) && (opcion != 3)){
+    alert("En dato ingresado es invalido. Ingrese 1, 2 o 3 según lo que quiere consultar");
+}
 
-//Salida 
-console.log("Hola" + " "+ nombre + " " + apellido + "! Según mis cálculos, tenes" + " " + edad + " " + "años.");
+//IVA 27%
+else if (opcion == 1){
+    precioSinIva = parseFloat(prompt("Ingrese el importe del producto para calcular el IVA del 27%"));
+    totalIva = (precioSinIva * iva27);
+    precioConIva = (totalIva + precioSinIva);
+
+    alert("El IVA es de $ " + totalIva + ". \nEl precio final a abonar con IVA es de $ " + precioConIva);
+}
+
+//IVA 21%
+else if (opcion == 2){
+    precioSinIva = parseFloat(prompt("Ingrese el importe del producto para calcular el IVA del 21%"));
+    totalIva = (precioSinIva * iva21);
+    precioConIva = (totalIva + precioSinIva);
+
+    alert("El IVA es de $ " + totalIva + ". \nEl precio final a abonar con IVA es de $ " + precioConIva);
+}
+
+//IVA 10,5%
+else{
+    precioSinIva = parseFloat(prompt("Ingrese el importe del producto para calcular el IVA del 10,5%"));
+    totalIva = (precioSinIva * iva10);
+    precioConIva = (totalIva + precioSinIva);
+
+    alert("El IVA es de $ " + totalIva + ". \nEl precio final a abonar con IVA es de $ " + precioConIva);
+}
