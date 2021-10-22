1f0f805af12dba2c22c1ff494c5bfa30a9e69a85
Camila Rojas <90579964+camilarojas11@users.noreply.github.com>
2021-10-22 19:28:44 -0300

    Modificación

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 //Variables
-let espacio = " ";
-let nombre = "";
-let apellido = "";
 let añoNacimiento = 0;
 let edad = 0;
 let resultado = 0;
 
 //Constante
-const mensaje = ("Hola! Por favor completa los siguientes datos.");
 const añoActual = 2021;
 
 
 //Ingreso de datos del usuario
-alert(mensaje);
-nombre = prompt("Ingrese su nombre");
-apellido = prompt("Ingrese su apellido");
+alert("Hola! Por favor completa los siguientes datos.");
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
 añoNacimiento = parseInt(prompt("Por favor ingrese el año en que nació (Ejemplo: 1994)"));
 
 //Proceso
 edad = añoActual - añoNacimiento;
 
 //Salida 
-console.log("Hola" + espacio + nombre + espacio + apellido + "! Según mis cálculos, tenes" + espacio + edad + espacio + "años.");
+console.log("Hola" + " "+ nombre + " " + apellido + "! Según mis cálculos, tenes" + " " + edad + " " + "años.");
