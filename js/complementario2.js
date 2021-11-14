9950068cfc45015f6a8aefd767276a670a226822
Camila Rojas <90579964+camilarojas11@users.noreply.github.com>
2021-11-14 18:11:43 -0300

    Agregue otro ejercicio

--- js/complementario2.js
+++ js/complementario2.js
@@ -7,3 +7,36 @@ for (let i = 1; i < 6; i++) {
 numeros.sort((a,b) => a - b);
 console.log(numeros);
 
+
+class Persona {
+    constructor (nombre, edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+}
+
+const personas = [];
+personas.push(new Persona("Camila", 27));
+personas.push(new Persona("Micaela", 27));
+personas.push(new Persona("Lucia", 26));
+personas.push(new Persona("Daiana", 25));
+personas.push(new Persona("Karen", 27));
+personas.push(new Persona("Mariana", 35));
+personas.push(new Persona("Sofía", 29));
+personas.push(new Persona("Yamila", 28));
+
+alert(`Vamos a ordenar a las personas según su edad, de menor a mayor.`)
+
+personas.sort((a, b) => {
+    if (a.edad < b.edad) {
+        return -1;
+    }
+
+    if (a.edad > b.edad) {
+        return 1
+    }
+
+    return 0;
+});
+
+console.log(personas);
